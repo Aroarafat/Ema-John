60620e63d2397f0ed8e7758b61feaa91b6f00b9c
Aroarafat <arafathossainbcpsc@gmail.com>
2021-09-23 18:02:44 +0600

    header-shopDetail-cartadded

---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react/cjs/react.development';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch('./products.JSON')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    products.map(product => <Product
+                        key={product.key}
+                        product={product}></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <h3>Order Summary</h3>
+                <h5>Order-item:</h5>
+            </div>
+        </div >
+    );
+};
+
+export default Shop;
